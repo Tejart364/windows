4f79906b27c86f1b9311d1d218b66a89df8ba204
Tejart364 <107937297+Tejart364@users.noreply.github.com>
2022-07-25 15:22:34 +0530

    Add files via upload

---
+++ Jagruthi script for horizon client 8.5.ps1
@@ -0,0 +1,13 @@
+﻿
+$ProcessCheck = (Get-Process -Name vmware-view -ErrorAction SilentlyContinue -ErrorVariable ProcessError)
+
+    if($ProcessCheck -eq $null)
+{
+exit
+}
+else
+{
+Start-Process "C:\Users\MMU8033\Desktop\PopUP_Message_OKButton.exe"-wait
+Stop-Process -Name Vmware-view -confirm:$false
+}
+   
